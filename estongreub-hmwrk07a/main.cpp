fdbe971be1c72a0013995bd1a9eb97dcc61aa091
Mariana Bolzani <bolzannni@gmail.com>
2019-04-06 22:50:36 -0700

    Initial Commit

---
+++ estongreub-hmwrk07a/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  estongreub-hmwrk07a
+//
+//  Created by MARIANA BOLZANI on 4/6/19.
+//  Copyright © 2019 MARIANA BOLZANI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
